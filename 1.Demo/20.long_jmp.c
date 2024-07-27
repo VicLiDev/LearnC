9a2e21af2d0b35351f6fc1e0f127836fde6258db
Hongjin Li <vic.hong@rock-chips.com>
2024-07-27 21:56:33 +0800

    update elf, c_language doc add long jump demo

---
+++ 1.Demo/20.long_jmp.c
@@ -0,0 +1,31 @@
+/*************************************************************************
+    > File Name: 20.long_jmp.c
+    > Author: LiHongjin
+    > Mail: 872648180@qq.com
+    > Created Time: Sat Jul 27 19:11:38 2024
+ ************************************************************************/
+
+/*
+ * longjmp的作用就是让程序的执行流回到当初setjmp返回的时刻，并且返回由longjmp指定
+ * 的返回值（参数2）
+ */
+
+#include <stdio.h>
+#include <setjmp.h>
+
+jmp_buf b;
+
+void m_func()
+{
+    longjmp(b, 1);
+}
+
+int main()
+{
+    if (setjmp(b))
+        printf("World!");
+    else {
+        printf("Hello ");
+        m_func();
+    }
+}
