eb667cb193e3e0a6d21c868d7d3ad0eee9ee08a9
Hongjin Li <vic.hong@rock-chips.com>
2024-11-26 12:26:13 +0800

    update getopt/getopt_long demo

--- 1.Demo/1.getopt/getopt.c
+++ 1.Demo/1.getopt/getopt.c
@@ -1,55 +1,90 @@
-/*
- * 参考博客:http://www.cnblogs.com/chenliyang/p/6633739.html
- * int getopt(int argc, char * const argv[], const char *optstring);
- * argc：main()函数传递过来的参数的个数
- * argv：main()函数传递过来的参数的字符串指针数组
- * optstring：选项字符串，告知 getopt()可以处理哪个选项以及哪个选项需要参数
- * 如果选项成功找到，返回选项字母；如果所有命令行选项都解析完毕，返回 -1；如果遇到选项字符不在 optstring 中，
- * 返回字符 '?'；如果遇到丢失参数，那么返回值依赖于 optstring 中第一个字符，如果第一个字符是 ':' 则返回':'，
- * 否则返回'?'并提示出错误信息。
- * 注意：在C语言中，单引号括起来的字符实际上代表一个整数，因此可以直接用 getopt 返回的 int 类型数据和单引号括起来的字母比较，
- *       例如代码示例中直接使用的 switch 语句
- *
- * 举例说明optstring的格式意义：
- * char *optstring = “ab:c::”;
- * 单个字符a         表示选项a没有参数           格式：-a即可，不加参数
- * 单字符加冒号b:     表示选项b有且必须加参数      格式：-b 100或-b100,但-b=100错
- * 单字符加2冒号c::   表示选项c可以有，也可以无    格式：-c200，其它格式错误
- * 上面这个 optstring 在传入之后，getopt 函数将依次检查命令行是否指定了 -a， -b， -c(这需要多次调用 getopt 函数，
- * 直到其返回-1)，当检查到上面某一个参数被指定时，函数会返回被指定的参数名称(即该字母)
- * optarg —— 指向当前选项参数(如果有)的指针。
- * optind —— 再次调用 getopt() 时的下一个 argv指针的索引。
- * optopt —— 最后一个未知选项。
- * opterr ­—— 如果不希望getopt()打印出错信息，则只要将全域变量opterr设为0即可。
- *
- * 注意: getopt是用来解析命令行选项参数的，但是只能解析短选项: -d 100,不能解析长选项：--prefix
- */
+/*************************************************************************
+    > File Name: getopt.c
+    > Author: LiHongjin
+    > Mail: 872648180@qq.com
+    > Created Time: Tue 26 Nov 12:16:49 2024
+ ************************************************************************/
 
-// usage: ./getopt -a10 -b 20 -c 30 -d
+/* usage: ./<exe> -v 12 -o moutput.txt */
 
-#include<stdio.h>
-#include<unistd.h>
-#include<getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
-int main(int argc, char **argv)
+
+typedef struct cmd_paras_t {
+    int  ver;
+    char *output_fname;
+} cmdParas;
+
+void print_usage(FILE* stream, int exit_code, const char *program_name)
 {
+    fprintf (stream, "Usage: %s options [ -o output_file ... ]\n", program_name);
+    fprintf (stream,
+             " -h Display this usage information.\n"
+             " -v Version messages.\n"
+             " -o Output filename.\n");
+    exit (exit_code);
+}
+
+int proc_paras(int argc, char **argv, cmdParas *parse_paras)
+{
+    const char* program_name = argv[0];
     int opt;
-    char *string = "a::b:c:d";
-    while ((opt = getopt(argc, argv, string))!= -1)
-    {
-        printf("opt = %c\t\t", opt);
-        printf("optarg = %s\t\t",optarg);
-        printf("optind = %d\t\t",optind);
-        printf("argv[optind] = %s\n",argv[optind]);
+    /* 单个字符a        表示选项a没有参数         格式：-a即可，不加参数 */
+    /* 单字符加冒号b:   表示选项b有且必须加参数   格式：-b 100或-b100,但-b=100错 */
+    /* 单字符加2冒号c:: 表示选项c可以有，也可以无 格式：-c200，其它格式错误 */
+    char *optstr = "hv:o:";
+
+    /*
+     * getopt
+     *
+     * getopt用来解析命令行参数，但只能解析短选项: -d 100,不能解析长选项：--prefix
+     *
+     * 返回值：
+     * getopt 返回匹配到的选项字符。如果选项需要参数，则参数值会保存在全局变量 optarg 中
+     * 如果遇到无效选项或缺少参数，getopt 返回 '?'
+     * 如果所有选项都被处理完，返回 -1
+     */
+    while ((opt = getopt(argc, argv, optstr))!= -1) {
+        /*
+         * optarg —— 指向当前选项参数(如果有)的指针
+         * optind —— 再次调用 getopt() 时的下一个 argv指针的索引
+         * optopt —— 最后一个未知选项
+         * opterr —— 如果不希望getopt()打印出错信息，则只要将全域变量opterr设为0即可
+         * printf("opt = %c\t\t", opt);
+         * printf("optarg = %s\t\t",optarg);
+         * printf("optind = %d\t\t",optind);
+         * printf("argv[optind] = %s\n",argv[optind]);
+         */
         switch(opt) {
-        case 'a':
-            printf("----------- a\n");
-            break;
-        case 'b':
-            printf("----------- b\n");
-            break;
-        default:
-            break;
+            case 'h':
+                print_usage (stdout, 0, program_name);
+                break;
+            case 'v':
+                parse_paras->ver = atoi(optarg);
+                break;
+            case 'o':
+                parse_paras->output_fname = optarg;
+                break;
+            case '?':
+                print_usage (stdout, 0, program_name);
+                break;
+            default: /* 别的什么:非预料中的 */
+                abort ();
         }
     }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    cmdParas mcmd_paras = {0, NULL};
+    proc_paras(argc, argv, &mcmd_paras);
+
+    printf("version  No: %d\n", mcmd_paras.ver);
+    printf("output file: %s\n",mcmd_paras.output_fname);
+    return 0;
 }
--- 1.Demo/1.getopt/getopt_long.c
+++ 1.Demo/1.getopt/getopt_long.c
@@ -1,34 +1,25 @@
-/*
- * int getopt_long(int argc, char * const argv[], const char *optstring, const struct option *longopts,int *longindex);
- * 描述：包含 getopt 功能，增加了解析长选项的功能如：--prefix --help
- * longopts    指明了长参数的名称和属性
- * longindex   如果longindex非空，它指向的变量将记录当前找到参数符合longopts里的第几个元素的描述，即是longopts的下标值
- * 对于短选项，返回值同getopt函数；对于长选项，如果flag是NULL，返回val，否则返回0；对于错误情况返回值同getopt函数
- *
- * struct option {
- * const char  *name;       // 参数名称,也是长选项的名称
- * int          has_arg;    // 指明是否带有参数,如果为 1 则表示该选项接受一个参数,否则为 0
- * int          *flag;      // flag=NULL时,函数返回val, 如下代码示例中： 可执行程序 --help, getopt_long 的返回值为 h;
- *                          // flag不为空时,可以设置一个变量，例如： {"http-proxy", required_argument, &lopt, 1 }, getopt_long返回0, lopt的值为1
- * int          val;        // 用于指定函数找到选项的返回值或flag非空时指定*flag的值
- * };
- *
- * has_arg  指明是否带参数值，其数值可选：
- * no_argument        表明长选项不带参数，如：--name, --help
- * required_argument  表明长选项必须带参数，如：--prefix /root或 --prefix=/root
- * optional_argument  表明长选项的参数是可选的，如：--help或 –prefix=/root，其它都是错误
- */
+/*************************************************************************
+    > File Name: getopt_long.c
+    > Author: LiHongjin
+    > Mail: 872648180@qq.com
+    > Created Time: Tue 26 Nov 11:44:19 2024
+ ************************************************************************/
+
+/* usage: ./<exe> --output o_file.txt -v hello */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
 
-/* 程序的名称。*/
-const char* program_name;
-/* 将程序使用方法输出到 STREAM 中(通常为 stdout 或 stderr),并以 EXIT_CODE 为返回值退出程序。函数调用不会返回。*/
-void print_usage (FILE* stream, int exit_code)
+
+typedef struct cmd_paras_t {
+    char *output;
+    int  verbose; /* 是否显示冗余信息?*/
+} cmdParas;
+
+void print_usage(FILE* stream, int exit_code, const char *program_name)
 {
-    fprintf (stream, "Usage: %s options [ inputfile ... ]\n", program_name);
+    fprintf (stream, "Usage: %s options [ -o|--output output_file ... ]\n", program_name);
     fprintf (stream,
              " -h --help Display this usage information.\n"
              " -o --output filename Write output to file.\n"
@@ -36,57 +27,77 @@ void print_usage (FILE* stream, int exit_code)
     exit (exit_code);
 }
 
-/* 程序主入口点。ARGC 包含了参数列表中元素的数量;ARGV 是指向这些参数的指针数组。*/
-int main (int argc, char* argv[])
+int proc_paras(int argc, char* argv[], cmdParas *parse_paras)
 {
-    int next_option;
-    /* 包含所有有效短选项字母的字符串。*/
+    const char* program_name = argv[0];
+    int opt;
+    /*
+     * 包含所有有效短选项字母的字符串
+     * 单个字符a        表示选项a没有参数         格式：-a即可，不加参数
+     * 单字符加冒号b:   表示选项b有且必须加参数   格式：-b 100或-b100,但-b=100错
+     * 单字符加2冒号c:: 表示选项c可以有，也可以无 格式：-c200，其它格式错误
+     */
     const char* const short_options = "ho:v";
     /* 描述了长选项的 struct option 数组。*/
+    /* *name;      长选项的名字（例如 "help"） */
+    /* has_arg;    是否有附加参数： */
+    /*               - no_argument (0): 无参数 */
+    /*               - required_argument (1): 必须有参数 */
+    /*               - optional_argument (2): 可选参数 */
+    /* *flag;      如果不为 NULL，则设置该指针的值 */
+    /* val;        如果 flag 为 NULL，则返回该值，便于后续case处理 */
     const struct option long_options[] = {
-        { "help", 0, NULL, 'h' },
-        { "output", 1, NULL, 'o' },
-        { "verbose", 0, NULL, 'v' },
-        { NULL, 0, NULL, 0 } /* 数组末要求这样一个元素。*/
+        { "help",    no_argument,       NULL, 'h' },
+        { "output",  required_argument, NULL, 'o' },
+        { "verbose", required_argument, NULL, 'v' },
+        { NULL, 0, NULL, 0 } /* 结束标志 */
     };
-    /* 用于接受程序输出的文件名,如果为 NULL 则表示标准输出。*/
-    const char* output_filename = NULL;
-    /* 是否显示冗余信息?*/
-    int verbose = 0;
-    /* 记住程序的名字,可以用于输出的信息。名称保存在 argv[0]中。*/
-    program_name = argv[0];
-    do {
-        next_option = getopt_long (argc, argv, short_options,
-                                   long_options, NULL);
-        switch (next_option) {
-        case 'h': /* -h 或 --help */
-            /* 用户要求查看使用帮助。输出到标准输出,退出程序并返回 0(正常结束)。*/
-            print_usage (stdout, 0);
-        case 'o': /* -o 或 --output */
-            /* 此函数接受一个参数,表示输出文件名。*/
-            output_filename = optarg;
-            (void) output_filename;
-            break;
-        case 'v': /* -v 或 --verbose */
-            verbose = 1;
-            break;
-        case '?': /* The user specified an invalid option. */
-            /* 向标准错误输出帮助信息,结束程序并返回 1(表示非正常退出)。*/
-            print_usage (stderr, 1);
-        case -1: /* 结束处理选项的过程。*/
-            break;
-        default: /* 别的什么:非预料中的。*/
-            abort ();
+
+    /*
+     * getopt_long:
+     * 如果成功解析一个选项，返回该选项对应的值。对于短选项，返回选项字符；对于长选项，返回 val 值。
+     * 如果没有更多选项可以解析，返回 -1。
+     * 如果遇到错误（例如无效的选项或缺少参数），返回 '?'。
+     *
+     * optarg 是一个全局变量，它在 getopt 和 getopt_long 函数解析命令行选项时，
+     * 用来保存当前选项的参数值。如果选项需要一个参数，那么该参数的值会被存储在
+     * optarg 中。
+     */
+    while ((opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'h': /* -h 或 --help */
+                print_usage (stdout, 0, program_name);
+                break;
+            case 'o': /* -o 或 --output */
+                parse_paras->output = optarg;
+                break;
+            case 'v': /* -v 或 --verbose */
+                parse_paras->verbose = 1;
+                break;
+            case '?':
+                print_usage (stderr, 1, program_name);
+                break;
+            default: /* 别的什么:非预料中的 */
+                abort ();
         }
     }
-    while (next_option != -1);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    cmdParas mcmd_paras = {NULL, 0};
+    proc_paras(argc, argv, &mcmd_paras);
+
     /* 选项处理完毕。OPTIND 指向第一个非选项参数。
      * 出于演示目的,如果指定了冗余输出选项,则输出这些参数。*/
-    if (verbose) {
+    printf("output: %s\n", mcmd_paras.output);
+    if (mcmd_paras.verbose) {
         int i;
         for (i = optind; i < argc; ++i)
             printf ("Argument: %s\n", argv[i]);
     }
-    /* 主程序到这里结束。*/
+
     return 0;
 }
